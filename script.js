3869c8839172770f0941dfc4787cfc0e96dd12b6
lefinsky <139684912+lefinsky@users.noreply.github.com>
2024-06-27 12:21:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-Kconst firstNames = ["KECOAK", "BONEKA", "", "SENDAL JEPIT", "Olivia", "KADAL SALTO", "ANTONY GANGSING", "ANKARA MESSI"];
-const lastNames = ["TERBANG", "MAMPANG", "PENCURI KOREK", "MIO MBERRR", "NGABERS", "JANGKRIK TAMAN", "BAU BAWANG", "JIN BOTAK"];
+Kconst firstNames = ["Kecoak", "Mampang", "Daun talas", "Sendal jepit", "Sunda", "Kadal salto", "ANTONY GANGSING", "ANKARA MESSI"];
+const lastNames = ["Terbang", "Mampang", "PENCURI KOREK", "MBERR", "NGABERS", "JANGKRIK TAMAN", "BAU BAWANG", "JIN BOTAK"];
 
 function getRandomName() {
     const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
